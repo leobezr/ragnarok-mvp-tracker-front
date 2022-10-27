fc1121855120c47bc0cddd58b17d5d173039ae24
BEZR <le.bezr@gmail.com>
2022-10-27 02:50:56 +0100

    feat<component>: create login form

--- src/views/anonymous/login/components/CreateLoginForm.vue
+++ src/views/anonymous/login/components/CreateLoginForm.vue
@@ -1,20 +1,65 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { createUser, rules } from "../lib/rules";
+import { Form } from "@/types/form";
+import { raiseSuccess, raiseError } from "@/lib/snackbar";
+import Utils from "@/lib/utils";
 
+const emit = defineEmits(["create", "toggle:stage"]);
 const CreateLoginForm = ref(null);
+
+const createUserAttempt = async () => {
+  const Form = Utils.isType<Form>(CreateLoginForm.value, "validate");
+
+  if (Form) {
+    await Form.validate((valid) => {
+      if (valid) {
+        emit("create", createUser);
+        raiseSuccess("Account successfully created!");
+      } else {
+        raiseError("Dumb dumb!");
+      }
+    });
+  }
+};
 </script>
 
 <template>
-  <el-form ref="CreateLoginForm">
+  <el-form :model="createUser" :rules="rules" ref="CreateLoginForm" status-icon>
     <h1>Create Account</h1>
 
-    <el-input placeholder="Email" />
+    <el-form-item prop="email">
+      <el-input
+        v-model="createUser.email"
+        placeholder="Email"
+        name="email"
+        type="email"
+      />
+    </el-form-item>
 
-    <el-input placeholder="Password" />
+    <el-form-item prop="password">
+      <el-input
+        v-model="createUser.password"
+        placeholder="Password"
+        name="password"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
 
-    <el-input placeholder="Confirm Password" />
+    <el-form-item prop="confirmPassword">
+      <el-input
+        v-model="createUser.confirmPassword"
+        placeholder="Confirm Password"
+        name="password"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
 
-    <el-button type="primary">Create Account</el-button>
+    <el-button @click="createUserAttempt" type="primary"
+      >Create Account</el-button
+    >
 
     <span class="createAccountCTA">
       <span>Oops! I already </span>
