ab035871c96ec659f1c5a8753fe6d5c546322483
BEZR <le.bezr@gmail.com>
2022-10-27 02:51:07 +0100

    feat<component>: login form

--- src/views/anonymous/login/components/LoginForm.vue
+++ src/views/anonymous/login/components/LoginForm.vue
@@ -1,21 +1,49 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { Form, Rules } from "@/types/form";
+import { ref, defineEmits } from "vue";
+import { Form } from "@/types/form";
+import { loginUser, rules } from "../lib/rules";
+import Utils from "@/lib/utils";
 
-const LoginForm = ref(null);
+const emit = defineEmits(["login", "toggle:stage"]);
+const LoginForm = ref<null | Form>(null);
 
-const validateForm = () => {};
+const loginAttempt = async () => {
+  const Form = Utils.isType<Form>(LoginForm.value, "validate");
+
+  if (Form) {
+    await Form.validate((valid) => {
+      if (valid) {
+        emit("login", loginUser);
+      }
+    });
+  }
+};
 </script>
 
 <template>
-  <el-form ref="LoginForm">
+  <el-form ref="LoginForm" :model="loginUser" :rules="rules">
     <h1>Welcome Back</h1>
 
-    <el-input placeholder="Email" />
+    <el-form-item prop="email">
+      <el-input
+        v-model="loginUser.email"
+        placeholder="Email"
+        name="email"
+        type="email"
+      />
+    </el-form-item>
 
-    <el-input placeholder="Password" />
+    <el-form-item prop="password">
+      <el-input
+        v-model="loginUser.password"
+        placeholder="Password"
+        name="password"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
 
-    <el-button type="primary">Login</el-button>
+    <el-button @click="loginAttempt" type="primary">Login</el-button>
 
     <span class="createAccountCTA">
       <span>Not a member yet?</span>
