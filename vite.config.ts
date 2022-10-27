dce5957ca733dcaeaa9eb7a1bbd2a271921c22cc
BEZR <le.bezr@gmail.com>
2022-10-27 01:55:46 +0100

    core<config>: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import { defineConfig } from "vite";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import ElementPlus from "unplugin-element-plus/vite";
-import vue from "@vitejs/plugin-vue";
+import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [
-    vue(),
+    Vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
