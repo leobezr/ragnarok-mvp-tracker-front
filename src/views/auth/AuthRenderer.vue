03b86f854c92a007563d2f11d390454ea421c519
BEZR <le.bezr@gmail.com>
2022-10-30 20:33:59 +0000

    feat: call sidebar component

--- src/views/auth/AuthRenderer.vue
+++ src/views/auth/AuthRenderer.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import SideBar from "@/global/sidebar/SideBarRoot.vue";
 </script>
 
 <template>
-  <router-view class="authenticated" />
+  <div class="connectedState">
+    <side-bar />
+
+    <router-view class="authenticated" />
+  </div>
 </template>
 
 <style lang="scss"></style>
