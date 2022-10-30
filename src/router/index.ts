0599a845835c4d22bc97de16b9b3ab59b8e0aada
BEZR <le.bezr@gmail.com>
2022-10-30 20:29:38 +0000

    feat: workaround to avoid dupplicated navigation alerts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,17 @@
-import { createWebHistory, createRouter } from "vue-router";
-import routes from "./routes"
+import { createWebHistory, createRouter, RouteLocationRaw } from "vue-router";
+import routes from "./routes";
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+export const navigate = (route: RouteLocationRaw) => {
+  try {
+    router.push(route);
+  } catch {
+    return;
+  }
+};
 
 export default router;
