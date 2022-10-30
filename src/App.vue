8348ddb4c7fadc5aea59b1bd33f7a0becc2d362d
BEZR <le.bezr@gmail.com>
2022-10-30 20:27:39 +0000

    chore: improvised session

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { eventBus } from "./main";
+import { navigate } from "@/router";
 import AuthRenderer from "./views/auth/AuthRenderer.vue";
 import "./style/style.scss";
 
-const userAuthenticated = ref(false);
+const authenticated = ref(true);
+
+const login = async (isAuthenticated: boolean) => {
+  authenticated.value = isAuthenticated;
+  navigate({ name: "Dashboard" });
+};
+
+eventBus.$on("authenticate", login);
 </script>
 
 <template>
   <div app-container>
-    <div v-if="userAuthenticated">
+    <div v-if="authenticated">
       <auth-renderer />
     </div>
 
     <router-view anonymous-session v-else />
   </div>
 </template>
-
-<style lang="scss"></style>
