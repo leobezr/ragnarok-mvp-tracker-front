64d821a25ac4dc75bf95734860b062f709f2777b
BEZR <le.bezr@gmail.com>
2022-11-05 14:06:49 +0000

    feat: lib update

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,20 @@
 import { Dictionary } from "@/types";
 
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Sep",
+  "Ago",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 export default class Utils {
   /**
    * This helper is perfect for using mutable unknown values.
@@ -29,5 +44,31 @@ export default class Utils {
     }
 
     return false;
+  }
+
+  public static countDown(from: number, to: number) {
+    const _t = (value: number) => `${value}`.padStart(2, "0");
+
+    if (to > from) {
+      const difRange = to - from;
+      const mins = Math.floor(difRange / (1000 * 60));
+      const ms = Math.ceil((difRange - 1000 * 60 * mins) / 1000);
+
+      return `${_t(mins)}:${_t(ms)}`;
+    }
+
+    return "0";
+  }
+
+  public static getDate(targetDate: number) {
+    const _t = (value: number) => `${value}`.padStart(2, "0");
+
+    const date = new Date(targetDate);
+    const day = _t(date.getDate());
+    const month = MONTHS[date.getMonth()];
+    const hour = _t(date.getHours());
+    const min = _t(date.getMinutes());
+
+    return `${month} ${day}, ${hour}h${min}`;
   }
 }
--- src/lib/utils/utils.spec.ts
+++ src/lib/utils/utils.spec.ts
@@ -18,4 +18,77 @@ describe("Testing Utils class", () => {
   it("Testing static method isType", () => {
     expect(Utils.isType(car, "")).toEqual(false);
   });
+
+  it("Testing method countDown, case 1", () => {
+    const second = 1000;
+    const min = second * 60;
+    const hour = min * 60;
+
+    const targetTime = Date.now() + hour * 1 + min * 10 + second * 52;
+    const now = Date.now();
+
+    expect(Utils.countDown(now, targetTime)).toEqual("70:52");
+  });
+
+  it("Testing method countDown, case 2", () => {
+    const second = 1000;
+    const min = second * 60;
+    const hour = min * 60;
+
+    const targetTime = Date.now() + hour * 5 + min * 10 + second * 5;
+    const now = Date.now();
+
+    expect(Utils.countDown(now, targetTime)).toEqual("310:05");
+  });
+
+  it("Testing method countDown, case 3", () => {
+    const second = 1000;
+    const min = second * 60;
+
+    const targetTime = Date.now() + min * 2 + second * 5;
+    const now = Date.now();
+
+    expect(Utils.countDown(now, targetTime)).toEqual("02:05");
+  });
+
+  it("Testing method countDown, case 4", () => {
+    const second = 1000;
+    const min = second * 60;
+
+    const targetTime = Date.now() + min * 10 + second * 0;
+    const now = Date.now();
+
+    expect(Utils.countDown(now, targetTime)).toEqual("10:00");
+  });
+
+  it("Testing method countDown, case 5", () => {
+    const targetTime = Date.now() - 1;
+    const now = Date.now();
+
+    expect(Utils.countDown(now, targetTime)).toEqual("0");
+  });
+
+  it("Testing method getDate, case 1", () => {
+    const targetDate = new Date("2021-10-19 19:10:00").getTime();
+
+    expect(Utils.getDate(targetDate)).toEqual("Oct 19, 19h10");
+  });
+
+  it("Testing method getDate, case 2", () => {
+    const targetDate = new Date("2021-12-31 00:10:00").getTime();
+
+    expect(Utils.getDate(targetDate)).toEqual("Dec 31, 00h10");
+  });
+
+  it("Testing method getDate, case 3", () => {
+    const targetDate = new Date("2022-01-01 00:10:00").getTime();
+
+    expect(Utils.getDate(targetDate)).toEqual("Jan 01, 00h10");
+  });
+
+  it("Testing method getDate, case 4", () => {
+    const targetDate = new Date("2022-04-05 05:10:00").getTime();
+
+    expect(Utils.getDate(targetDate)).toEqual("Apr 05, 05h10");
+  });
 });
