c4ea3d0f8dabf66aab47c2c7473e1a5c173468a2
BEZR <le.bezr@gmail.com>
2022-11-05 14:08:25 +0000

    core<component>: global component for app card

---
+++ src/global/app-card/AppCardRoot.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+import "./scss/_app-card.scss";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <div class="appCardRoot">
+    <h3 v-if="props.title">{{ props.title }}</h3>
+    <slot />
+  </div>
+</template>
