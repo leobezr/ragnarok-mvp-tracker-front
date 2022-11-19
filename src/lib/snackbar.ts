201f3e7b7a19a4c56274996995f54f8875fcce43
BEZR <le.bezr@gmail.com>
2022-11-19 19:41:52 +0000

    feat: app alert system

--- src/lib/snackbar.ts
+++ src/lib/snackbar.ts
@@ -1,23 +1,26 @@
 import { ElMessage } from "element-plus";
 
-export const raiseSuccess = (message?: string) => {
+export const raiseSuccess = (message?: string, duration = 2000) => {
   ElMessage({
     ...(message && { message }),
     type: "success",
+    duration,
   });
 };
 
-export const raiseError = (message?: string) => {
+export const raiseError = (message?: string, duration = 2000) => {
   ElMessage({
     ...(message && { message }),
     type: "error",
+    duration,
   });
 };
 
-export const raiseWarning = (message?: string) => {
+export const raiseWarning = (message?: string, duration = 2000) => {
   ElMessage({
     ...(message && { message }),
     type: "warning",
+    duration,
   });
 };
 
