0f75d3db61ef1c85d517ab4a77749240284fadac
BEZR <le.bezr@gmail.com>
2022-11-19 19:40:18 +0000

    feat: global app card with hover and click effect

--- src/global/app-card/AppCardRoot.vue
+++ src/global/app-card/AppCardRoot.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
+import LoaderProgress from "../loader-progress/LoaderProgress.vue";
 import "./scss/_app-card.scss";
 
 const props = defineProps({
@@ -7,12 +8,33 @@ const props = defineProps({
     type: String,
     required: false,
   },
+  hoverEffect: {
+    type: Boolean,
+    default: false,
+  },
+  loader: {
+    type: Boolean,
+    default: false,
+  },
 });
 </script>
 
 <template>
-  <div class="appCardRoot">
-    <h3 v-if="props.title">{{ props.title }}</h3>
-    <slot />
+  <div
+    @click="$emit('click')"
+    class="appCardRoot"
+    :hover-effect="props.hoverEffect"
+  >
+    <slot name="header">
+      <div class="cardHeader">
+        <h3 v-if="props.title">{{ props.title }}</h3>
+      </div>
+    </slot>
+
+    <loader-progress v-if="loader" />
+
+    <div class="mainCardContent">
+      <slot />
+    </div>
   </div>
 </template>
