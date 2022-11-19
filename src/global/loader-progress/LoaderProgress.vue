070a7e91fed79c51d7b261efd2444dbce0f83810
BEZR <le.bezr@gmail.com>
2022-11-19 19:40:39 +0000

    feat: generic loader

---
+++ src/global/loader-progress/LoaderProgress.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps({
+  duration: {
+    type: Number,
+    default: 0.8,
+  },
+  status: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <el-progress
+    :duration="props.duration"
+    :status="props.status"
+    :indeterminate="true"
+    :percentage="100"
+    :text-inside="true"
+    :format="() => ''"
+    :stroke-width="3"
+  />
+</template>
